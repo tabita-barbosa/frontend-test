2a45357dc3ce041bb8e785c1918b12f2e0a15d39
Tabita Barbosa <tabita.barbosa8@gmail.com>
2020-11-10 02:19:21 -0300

    estrutura html, desenvolver funções js e css

---
+++ script.js
@@ -0,0 +1,37 @@
+// fazer um map que pegue image, title, subtitle e text e popule nos itens já existentes no HTML
+// pegar pela class 
+
+// const slidesFinal = document.querySelectorByClass ('.slides')
+// const imagem = document.querySelector('img')
+// const title = document.querySelector('h1')
+// const subtitle = document.querySelector('h3')
+// const paragraph = document.querySelector('p')
+
+// const dados = JSON.parse("./slides.json")
+// console.log(dados)
+
+// const getDados = dados.map((slides) =>{
+//     document.createElement(img)
+// })
+// // console.log(slidesFinal)
+// function Slider (image, title, subtitle, text){
+//     this.image = image,
+//     this.title = title;
+//     this.subtitle = subtitle;
+//     this.text = text
+// }
+
+const name = document.querySelector('.name')
+const born = document.querySelector('.born')
+const driverNum = document.querySelector('.driver-license-num')
+const driverDate = document.querySelector('.driver-license-date')
+const state = document.querySelector('.state')
+const city = document.querySelector('.city')
+const tel = document.querySelector('.tel')
+const email = document.querySelector('.email')
+const adicionarMais = document.querySelector('.button-add')
+const respName = document.querySelector ('.respName')
+const respTel = document.querySelector('.respTel')
+const saveClient = document.querySelector('.saveClient')
+
+
